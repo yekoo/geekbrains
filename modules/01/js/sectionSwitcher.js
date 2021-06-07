5f788e3727fccd477486240d539708396bc87627
yekoo <extisque@yandex.ru>
2021-06-07 20:16:49 +0300

    Update sectionSwitcher.js

--- modules/01/js/sectionSwitcher.js
+++ modules/01/js/sectionSwitcher.js
@@ -1,1 +1,23 @@
 
+const sections = [    
+    {
+        id:"dateDiff",
+        name:"Date difference"
+    },
+    {
+        id:"stepwatch",
+        name:"Step watch"
+    },
+    {
+        id:"timer",
+        name:"Timer"
+    }
+];
+let header = document.querySelector("header");
+console.log(header);
+const linksList = document.createElement("ul");
+for(let itm of sections){
+    console.log(itm);
+    linksList.insertAdjacentHTML('beforeend', `<li><a href="#${itm.id}">${itm.name}</a></li>`);
+}
+header.appendChild(linksList);
